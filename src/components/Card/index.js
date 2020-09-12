bc878f00a60c62894e5b934b13db3de06a82b304
Casey Carroll <caseydean8@gmail.com>
2020-09-12 17:35:28 -0400

    Fixed button opacity issue in Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,19 +4,24 @@ import CardBtn from "../CardBtn";
 import CardImg from "../CardImage";
 import CardHeading from "../CardHeading";
 import "./style.css";
+import ContextAPI from "../../utils/ContextAPI";
+ 
 
 function Card(props) {
   return (
     <div>
-      <CardHeading title={props.title} />
-      <CardImg image={props.image} />
-      <CardBody language={props.language} email={props.email} />
-      {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
-      <CardBtn
-        style={{ opacity: props.image ? 1 : 0 }}
-        onClick={props.handleBtnClick}
-        data-value="back"
-      />
+      <CardHeading />
+      <CardImg />
+      <CardBody />
+      <ContextAPI>
+        {context => (
+           <CardBtn
+           style={{ opacity: context.user.image ? 1 : 0 }}
+           onClick={props.handleBtnClick}
+           data-value="back"
+         />
+        )}
+      </ContextAPI>
       <CardBtn
         style={{ opacity: props.image ? 1 : 0 }}
         onClick={props.handleBtnClick}
--- src/components/CardBtn/index.js
+++ src/components/CardBtn/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 
 function CardBtn(props) {
+  console.log('props in CardBtn', props);
   return (
     <button onClick={props.onClick} className={`card-btn ${props["data-value"]}`} {...props} />
   );
